a813109afa87dd905f59323c681b93dc156b6908
Sara Burke <danielaburkereyes@gmail.com>
2022-09-29 18:31:00 -0400

    Hw 3 in Completed

---
+++ module1.py
@@ -0,0 +1,15 @@
+#1) Has a function to calculate the square footage of a house
+# Reminder of Formula: Length X Width == Area
+def areaofsquare(lenght, width):
+    return lenght * width
+    
+print(areaofsquare())   
+
+
+# 2) Has a function to calculate the circumference of a circle
+
+def circum(radius):
+    return 2 * 3.14159 * radius
+
+print(circum())    
+
